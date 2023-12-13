8a83d70a7d9c6f70f724149e24250882e6b30691
revi-hub <150064623+revi-hub@users.noreply.github.com>
2023-12-13 19:38:27 +0100

    Complete 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('')
+    let stringLen = string.length
+    let buffer 
+    for(let i = 0, j = stringLen - 1; i <= Math.floor((stringLen - 1) / 2); i++, j--){
+        buffer = stringArray[i]
+        stringArray[i] = stringArray[j]
+        stringArray[j] = buffer
+    }
+    return stringArray.join('')
 };
-
-// Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString; 
