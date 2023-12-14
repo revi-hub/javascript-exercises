bab3d2e897a0699859213a62384f9db8d8c26ea3
revi-hub <150064623+revi-hub@users.noreply.github.com>
2023-12-14 20:56:40 +0100

    Complete 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheits) {
+  let celsius = ((fahrenheits - 32) * 5/9);
+  if(!Number.isInteger(celsius)) return +celsius.toFixed(1);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius * 9/5 + 32);
+  if(!Number.isInteger(fahrenheit)) return +fahrenheit.toFixed(1)
+  return fahrenheit
 };
 
 // Do not edit below this line
