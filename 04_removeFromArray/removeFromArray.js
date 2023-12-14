8d46d0b2b79a105bcee71ec241ae10859445340a
revi-hub <150064623+revi-hub@users.noreply.github.com>
2023-12-14 19:05:45 +0100

    Complete 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsToDelete) {
+    for(let i = 0; i < itemsToDelete.length; i++){
+        if(array.includes(itemsToDelete[i])){
+            let itemToDeleteIndex = array.indexOf(itemsToDelete[i])
+            let arrayPartLeft = array.slice(0, itemToDeleteIndex)
+            let arrayPartRight = array.slice(itemToDeleteIndex + 1, array.length)
+            array = arrayPartLeft.concat(arrayPartRight)
+        }
+    }
+    return array
+    
 };
 
 // Do not edit below this line
