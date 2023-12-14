c3ec935fd68edd6bb1671dde69ed236b668f7593
revi-hub <150064623+revi-hub@users.noreply.github.com>
2023-12-14 19:43:26 +0100

    Complete 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a, b) {
+ if((typeof(a) != 'number') || (typeof(b) != 'number')) return 'ERROR';
+ if(a < 0 || b < 0) return 'ERROR'
+ let initValue, endValue;
+ let sum = 0;
+ if (a > b) {
+    initValue = b;
+    endValue = a;
+}
+ else {
+    initValue = a;
+    endValue = b;
+ }
+ for (let i = initValue; i <= endValue; i++) {
+    sum += i;
+ }
+ return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
