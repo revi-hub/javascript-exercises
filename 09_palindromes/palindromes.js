5a6c7c15e87693c44545ba4a17b11f170664a40a
Revi <150064623+revi-hub@users.noreply.github.com>
2024-05-01 15:13:17 +0200

    Complete 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.toLowerCase().split('').filter( ( value ) => {
+        switch (value) {
+            case '.':
+                return false;
+                break;
+            case ',':
+                return false;
+                break;   
+            case '!':
+                return false;
+                break;   
+            case '?':
+                return false;
+                break;
+            case ' ':
+                return false;
+                break;        
+            default:
+                return true;
+                break;
+        }
+    })
+    let strFiltered = arr.join('')
+    let strFilteredReverse = arr.reverse().join('')
+    console.log(strFiltered)
+    return strFiltered == strFilteredReverse;
 };
 
 // Do not edit below this line
