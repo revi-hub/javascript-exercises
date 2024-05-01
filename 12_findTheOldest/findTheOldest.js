ad75597a9f9519a849c0f7d63492c789688eba04
Revi <150064623+revi-hub@users.noreply.github.com>
2024-05-01 16:32:07 +0200

    Complete 12 exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+
+    if(!person.hasOwnProperty('yearOfDeath'))
+        return new Date().getFullYear() - person.yearOfBirth
+    else
+        return person.yearOfDeath - person.yearOfBirth
+
+};
+
+
+const findTheOldest = function(people) {
+
+return people.reduce( (oldest, curr) => getAge(curr) > getAge(oldest) ? curr : oldest)
 
 };
 
