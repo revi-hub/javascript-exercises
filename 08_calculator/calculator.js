1294eaa75e96b49ba576f68915aecb5de97000ca
Revi <150064623+revi-hub@users.noreply.github.com>
2024-05-01 14:11:31 +0200

    Complete 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (total, curr) => total * curr);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+  for(let i = 1; i < b; i++){
+    result = result * a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0)
+  return 1;
+let result = 1;
+	for(let i = 2; i <= a; i++){
+    result *= i
+  }
+  return result;
 };
 
 // Do not edit below this line
