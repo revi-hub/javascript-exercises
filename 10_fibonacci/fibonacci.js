02265f3eaf3ede69a3600a80b3f473dbc072521a
Revi <150064623+revi-hub@users.noreply.github.com>
2024-05-01 15:50:39 +0200

    Complete 10 exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prev = 1
+    let curr = 1
+    let variable = 0
 
+    if(num == 0)
+        return 0
+
+    else if(num < 0){
+        return 'OOPS'
+    }
+
+    for(let i = 2; i < num; i++){
+        variable = curr;
+        curr = prev + curr;
+        prev = variable;
+    }
+
+    return curr
+    
 };
 
 // Do not edit below this line
